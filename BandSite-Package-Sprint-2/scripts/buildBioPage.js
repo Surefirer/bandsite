fb0e412924bbf77ccf11e1ffbc416e584e4178ed
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-04-30 18:52:15 -0700

    20200430 updates

--- BandSite-Package-Sprint-2/scripts/buildBioPage.js
+++ BandSite-Package-Sprint-2/scripts/buildBioPage.js
@@ -1,54 +1,34 @@
+// referencing DOM elements
+let container = document.querySelector(".commentList");
+let liveCommentForm = document.querySelector(".liveCommentForm");
+
+// add event listerner
+liveCommentForm.addEventListener("submit", handleFormSubmit);
+
+// customers name and comment array
 const customers = [{
         name: "Micheal Lyons",
         comment: "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
+        date: new Date("12/18/2018"),
     },
     {
         name: "Gary Wong",
-        comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's talented! I wish I can ride like him one day so I can really enjoy myself!"
+        comment: "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He's talented! I wish I can ride like him one day so I can really enjoy myself!",
+        date: new Date("12/12/2018"),
     },
     {
         name: "Theodore Duncan",
         comment: "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He's definitely my favorite ever!",
+        date: new Date("11/15/2018"),
     },
 ];
 
-console.table(customers);
+//console.table(customers); --- for test only
 
-const container = document.querySelector(".comment");
-
-// const commentTitle = document.createElement("h2");
-// commentTitle.innerHTML = "Join the Conversation";
-
-// const liveCommentName = document.createElement("h4");
-// liveCommentName.innerHTML = "NAME";
-
-// const nameTextBox = document.createElement("input");
-// nameTextBox.classList.add("nameTextBox");
-
-// nameTextBox.setAttribute("placeholder", "dkkdkdk");
-
-// const nameTextBoxPlaceHolder = document.querySelector(".nameTextBox").placeholder = "kkk";
-// document.getElementsByTagName("input").setAttribute("id", "nameTextBox");
-// nameTextBox.innerHTML = "Mohan Muruge";
-
-// const liveCommentTitle = document.createElement("h4");
-// liveCommentTitle.innerHTML = "COMMENT";
-
-// const liveCommentBox = document.createElement("textarea");
-
-// const liveCommentBtn = document.createElement("button");
-// liveCommentBox.classList.add("liveCommentBtn");
-// liveCommentBtn.innerHTML = "SUBMIT";
-
-// container.appendChild(commentTitle);
-// container.appendChild(liveCommentName);
-// container.appendChild(nameTextBox);
-// container.appendChild(liveCommentTitle);
-// container.appendChild(liveCommentBox);
-// container.appendChild(liveCommentBtn);
-
-
+// display&list of comments in page
 function constructComments(customers) {
+    container.innerHTML = "";
+
     for (customer of customers) {
         const showComments = document.createElement("div");
         showComments.classList.add("showComments");
@@ -56,10 +36,14 @@ function constructComments(customers) {
         const name = document.createElement("h4");
         name.innerHTML = customer.name;
 
+        const date = document.createElement("time");
+        date.innerHTML = customer.date.getDay() + "-" + customer.date.getMonth() + "-" + customer.date.getFullYear();
+
         const comment = document.createElement("p");
         comment.innerHTML = customer.comment;
 
         showComments.appendChild(name);
+        showComments.appendChild(date);
         showComments.appendChild(comment);
 
         container.appendChild(showComments);
@@ -67,3 +51,19 @@ function constructComments(customers) {
 }
 
 constructComments(customers);
+
+// function for form submit and add new comment
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const userNameVal = event.target.userName.value;
+    const userCommentsVal = event.target.userComments.value;
+
+    customers.push({
+        name: userNameVal,
+        comment: userCommentsVal,
+        date: new Date(),
+    });
+
+    constructComments(customers);
+}
